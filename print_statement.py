8642734d9b8817dc56b9a5d4d9d1fd35c29c7fc7
CodingTutorials-github <99127615+CodingTutorials-github@users.noreply.github.com>
2022-02-06 21:24:45 +0800

    Create print_statement.py

---
+++ print_statement.py
@@ -0,0 +1,9 @@
+print("hello world")
+print("I am BC from coding tutorials")
+print("AH-hoy!")
+
+#this is the simple way to print HELLO WORLD
+
+print("hello", "world")
+print("hello" "world")
+#this are other ways to print HELLO WORLD
