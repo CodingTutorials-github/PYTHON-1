6f98d6f4f41b8e172a4f1967cf0d96ceb7716395
CodingTutorials-github <99127615+CodingTutorials-github@users.noreply.github.com>
2022-02-11 20:54:36 +0800

    Create pyMATH.py

---
+++ pyMATH.py
@@ -0,0 +1,25 @@
+#math ADDITION
+numOranges = 60
+numApples = 35
+totalFruits = numOranges + numApples
+print(totalFruits)
+#math SUBTRACTION
+numOranges = 50
+numApples = 27
+extraOrange = numOranges - numApples
+print(extraOrange)
+#math DIVISION
+a = 373535444
+b = 7573847
+c = a/b
+print (c)
+#math MULTIPLICATION
+x = 7
+y = 3
+c = x + y
+print (c)
+#so far we have concluded how to express the 4 operations in math by using PYTHON 3.9.5
+#Now we will move on to something to the power of something e.g.: 3 to the power of 5 = 3x3x3x3x3
+#i npython, the _to_ we use it as: _**_ so example 5 to 6 in math and in python difference:
+#IN MATH: 5 to the power of 6
+#IN CODE: 5**6
