d9350d9b78048c9c33423701211646f0db0ca3ad
CodingTutorials-github <99127615+CodingTutorials-github@users.noreply.github.com>
2022-02-11 20:55:03 +0800

    Create casting.py

---
+++ casting.py
@@ -0,0 +1,22 @@
+#type 1 of defining hello78
+myGreeting= 'hello'
+age = 78
+print (myGreeting + str(age))
+#type 2 of defining hello 78
+myGreetings = 'hello'
+age = "78"
+print(myGreetings + age)
+
+
+#casting does not permently change the data type. It only changes wherever it is used.
+#str() converts whatever is inside the bracket to a string
+
+
+#for now,... LET'S PLAY AROUND WITH CASTING!
+age = 78
+temp = 56.445
+print (age)
+print (float(age))
+print (str(age))
+print (int(temp))
+print(int(6))
