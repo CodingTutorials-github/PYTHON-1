7c28cc0805e0f3aa6f9b62f01c6a99a8ddad00b7
CodingTutorials-github <99127615+CodingTutorials-github@users.noreply.github.com>
2022-02-11 21:36:45 +0800

    Create revise1.py

---
+++ revise1.py
@@ -0,0 +1,32 @@
+greet=input('LETS START ON A MATH QUESTION!')
+#starting message
+if greet == "ok":
+  #if user wants to do quiz, then give the questions.
+  print("LETS START")
+  question_ans = input("question is, what is 6+44?") #this is the question
+  if question_ans == "50": #if answer is correct answer, print thats correct
+    print("thats correct!")
+  else:
+    print("thats wrong, please try again") # if ans is wrong (not 50) print the message
+else:
+  print("ok......") #if user types other things instead of ok in if greet == "ok":
+  
+  __________________________________________________________________________________________________________-
+  
+  good = ""
+function1 = input("what is your name?")
+# this serves as the robot asking you your name, and there will be a box behind its speech for you to reply
+function2 = input("oooo, hi"+ function1+ "how are you?><")
+# this , name, means no matter what name you entered for function 1 it will be displayed in function2  speech
+if function2 == "good":  
+  print("okie")
+else:
+  print("nah")
+# means, if you tye good, it will say thats good to hear. Other wise, if you type anything
+# else, E.G.: hi, bad, meh, it will broadcast message "er ok"
+
+
+
+
+
+# you can change function 1 to name, function 2 to hi, all the other name, vice versa
