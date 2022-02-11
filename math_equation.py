f2fdef8253cf22719b3e2db84428b8010a6e6a2c
CodingTutorials-github <99127615+CodingTutorials-github@users.noreply.github.com>
2022-02-11 21:01:17 +0800

    Create math_equation.py

---
+++ math_equation.py
@@ -0,0 +1,15 @@
+print(8%59)   #gives you the remainder when 5 is divided by 2
+print(8%59)   #the remainder is 2
+print(6%4)    #the remainder is 8
+
+
+print(43473%359)
+print(753/2)
+print(0.78*32)
+print(62.5/4)
+print(16**3)
+print(7**6)
+print(654*1234)
+
+x=345.87
+b= 536
