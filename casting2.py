44c64c944eab419e3cdb42e915c467e569fb1901
CodingTutorials-github <99127615+CodingTutorials-github@users.noreply.github.com>
2022-02-11 20:55:24 +0800

    Create casting2.py

---
+++ casting2.py
@@ -0,0 +1,5 @@
+#ask user for a value of numChickens variable, and remember to make use of int()
+#By default the value of the input() function assigns to the variable of the data type string()
+numChickens = int(input("how many chickens?"))
+numFeet = numChickens*2
+print ("there are " + str(numFeet) + " feet in total")
